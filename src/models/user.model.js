76475f68b372bdff524437331d7446e2a55d1eac
Escape <luckylilhore123@gmail.com>
2025-02-28 16:09:41 +0530

    authentication and hashing part is complete in userModel.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
      username:{
@@ -23,6 +25,36 @@ const userSchema = new mongoose.Schema({
           type: mongoose.Schema.Types.ObjectId,
           ref: 'Booking',
      }]
+},
+{
+     timestamps: true,
 });
+
+userSchema.methods.generateAuthToken = ()=>{
+     const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET, {expiresIn: '5d'});
+     return token;
+}
+
+userSchema.statics.authenticate = async(email, password)=>{
+     const user = await this.findOne({email}).select('+password');
+     if(!user){
+          throw new Error('Invalid email or password');
+     }
+
+     const isMatch = await bcrypt.compare(password, user.password);
+     if(!isMatch){
+          throw new Error('Invalid email or password');
+     }
+
+     return user;
+}
+
+userSchema.pre('save', async (next)=>{
+     if(this.isModified('password')){
+          this.password = await bcrypt.hash(this.password);
+     }
+     next();
+})
+
 
 module.exports = mongoose.model('User', userSchema)
